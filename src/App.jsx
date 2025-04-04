9bf3f1956529f1198ff3a994dd1c708d01b6f0e4
Brian <brian95gsus@gmail.com>
2025-04-04 12:35:20 -0400

    primercomit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+const Hello = () => {
+  return(
+    <div>
+       <p> Hola gente</p>
+       </div>
+   
+  )
+}
+const App = () => {
+  return (
+    <div>
+      <h1> Gretings</h1>
+      <Hello/>
+    </div>
+  )
+}
+
+
+export default App
