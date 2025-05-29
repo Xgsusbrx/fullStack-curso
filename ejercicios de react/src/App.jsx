0e83846988e0845731abad8428396780c837d657
Brian <brian95gsus@gmail.com>
2025-05-29 02:01:00 -0400

    primera version

--- ejercicios de react/src/App.jsx
+++ ejercicios de react/src/App.jsx
@@ -1,16 +1,69 @@
-const App = (props) => {
-  const { notes } = props
+const App = () => {
+  
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+      
+    ]
+    
+  }
+ const total = course.parts.reduce((acumulador,EstadoActual) => {
+return acumulador+=EstadoActual.exercises
 
+ }, 0)
+     
   return (
-    <div>
-      <h1>Notes</h1>
-      <ul>
-        <li>{notes[0].content}</li>
-        <li>{notes[1].content}</li>
-        <li>{notes[2].content}</li>
-      </ul>
-    </div>
-  )
+  <div>
+    
+    <Course course={course} />
+    <Header course={course} />
+    <p>Total of {total} exercises</p>
+    
+  </div>        
+  )  
 }
+const Course = ({course}) => {
+
+    return (
+        <div>
+            <h1>{course.name}</h1>   
+        </div>
+    )
+}
+
+const Header = ({course}) => { 
+
+ return (
+    <div>
+     {course.parts.map(date =>
+      <p key = {date.id} >
+        {date.name} {date.exercises}
+      </p>
+      
+     )}
+    </div>
+    
+  )
+
+}
+
+
+  
 
 export default App
